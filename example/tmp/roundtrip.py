9c4d8a399e2bb89a45e3ff833b9849ca7292f2eb
hhland <hhlin54133@gmail.com>
2014-12-08 16:30:08 +0800

    ....

---
+++ example/tmp/roundtrip.py
@@ -0,0 +1,8 @@
+from oct2py import octave
+import numpy as np
+
+if __name__ == '__main__':
+    x = np.array([[1, 2], [3, 4]], dtype=float)
+    out, oclass = octave.roundtrip(x)
+    import pprint
+    pprint.pprint([x, x.dtype, out, oclass, out.dtype])
